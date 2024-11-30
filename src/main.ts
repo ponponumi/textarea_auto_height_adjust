f700d0b136214c512d649f459bcd0984bbc1efca
ponponumi <ponponumi@gmail.com>
2024-11-30 15:01:32 +0900

    数値に変換するメソッドを使うように変更

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,22 @@ class TextareaAutoHeightAddjust{
         // 要素を判定する
         console.log(elem);
         let height = getComputedStyle(elem).getPropertyValue("height");
-        height = height.replace(/[^0-9.]/g, '');
+        let heightNum: number = this.numberConvert(height);
 
         // パディングを取得
         let paddingTop = getComputedStyle(elem).getPropertyValue("padding-top");
         let paddingBottom = getComputedStyle(elem).getPropertyValue("padding-bottom");
 
-        paddingTop = paddingTop.replace(/[^0-9.]/g, '');
-        paddingBottom = paddingBottom.replace(/[^0-9.]/g, '');
+        let paddingTopNum: number = this.numberConvert(paddingTop);
+        let paddingBottomNum: number = this.numberConvert(paddingBottom);
 
         // スクロールの高さを取得
         let scrollHeight = elem.scrollHeight;
 
         let test: object = {
-            "height": height,
-            "paddingTop": paddingTop,
-            "paddingBottom": paddingBottom,
+            "height": heightNum,
+            "paddingTop": paddingTopNum,
+            "paddingBottom": paddingBottomNum,
             "scrollHeight": scrollHeight,
         };
 
