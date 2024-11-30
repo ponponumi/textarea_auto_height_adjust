f8b7033a8c365564dd95cc88ab71c1fb9b75dc5c
ponponumi <ponponumi@gmail.com>
2024-11-30 14:35:53 +0900

    調整用のメソッドをまずは作成

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+class TextareaAutoHeightAddjust{
+    addjustElem(elem: HTMLElement | null | undefined) {
+        // 要素を判定する
+        let elemCheck = elem instanceof HTMLElement;
+
+        if (elemCheck) {
+            console.log(elem);
+        } else {
+            console.log("これはHTML要素ではありません");
+        }
+    }
+}
