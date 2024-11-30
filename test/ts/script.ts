dce03d834d6ba34987f53cdaea08a98065620a0e
ponponumi <ponponumi@gmail.com>
2024-12-01 01:10:10 +0900

    テストコードを修正

--- test/ts/script.ts
+++ test/ts/script.ts
@@ -2,7 +2,9 @@ import AutoAdjust from "../../src/main";
 
 // AutoAdjust.addjustElem(document.getElementById("message"));
 // AutoAdjust.addjustElem(document.getElementById("messageElem"));
-AutoAdjust.addjustAll(function (check) {
+AutoAdjust.addjustAll(function (check, styleData) {
+    console.log(styleData);
+
     if (check) {
         console.log("高さが変わりました");
     } else {
