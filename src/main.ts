7211fd6725a01a7d260b445598c5bbf87e97b7fa
ponponumi <ponponumi@gmail.com>
2024-11-30 14:58:11 +0900

    ドットも残すように変更

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ class TextareaAutoHeightAddjust{
         // 要素を判定する
         console.log(elem);
         let height = getComputedStyle(elem).getPropertyValue("height");
-        height = height.replace(/\D/g, '');
+        height = height.replace(/[^0-9.]/g, '');
 
         // パディングを取得
         let paddingTop = getComputedStyle(elem).getPropertyValue("padding-top");
         let paddingBottom = getComputedStyle(elem).getPropertyValue("padding-bottom");
 
-        paddingTop = paddingTop.replace(/\D/g, '');
-        paddingBottom = paddingBottom.replace(/\D/g, '');
+        paddingTop = paddingTop.replace(/[^0-9.]/g, '');
+        paddingBottom = paddingBottom.replace(/[^0-9.]/g, '');
 
         // スクロールの高さを取得
         let scrollHeight = elem.scrollHeight;
