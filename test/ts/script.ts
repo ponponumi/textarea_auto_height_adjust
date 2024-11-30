e1c08a65b7e83f3cc55469fb2818d99107209c1d
ponponumi <ponponumi@gmail.com>
2024-11-30 15:40:52 +0900

    textarea以外の場合は動作しない事を確認

--- test/ts/script.ts
+++ test/ts/script.ts
@@ -3,3 +3,4 @@ import AutoAdjust from "../../src/main";
 // AutoAdjust.addjustElem(document.getElementById("message"));
 // AutoAdjust.addjustElem(document.getElementById("messageElem"));
 AutoAdjust.addjustAll();
+AutoAdjust.addjustId("name");
