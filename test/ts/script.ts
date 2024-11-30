3de1c45193001c049542ec0596d798031dde23ce
ponponumi <ponponumi@gmail.com>
2024-12-01 01:05:36 +0900

    なぜか常に「高さが変わりました」と出るバグが発生

--- test/ts/script.ts
+++ test/ts/script.ts
@@ -2,5 +2,10 @@ import AutoAdjust from "../../src/main";
 
 // AutoAdjust.addjustElem(document.getElementById("message"));
 // AutoAdjust.addjustElem(document.getElementById("messageElem"));
-AutoAdjust.addjustAll();
-AutoAdjust.addjustId("name");
+AutoAdjust.addjustAll(function (check) {
+    if (check) {
+        console.log("高さが変わりました");
+    } else {
+        console.log("高さが変わりませんでした");
+    }
+});
