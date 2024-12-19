af27ba7044e180ca8c0965cd416f977fd681f9e3
ponponumi <ponponumi@gmail.com>
2024-12-19 13:44:11 +0900

    adjustAllメソッドが動作することを確認

--- test/ts/script.ts
+++ test/ts/script.ts
@@ -10,11 +10,10 @@ declare global {
 window.AutoAdjust = AutoAdjust;
 
 // AutoAdjust.adjustElem(documenst.getElementById("message"));
-AutoAdjust.adjustId("message");
+// AutoAdjust.adjustId("message");
 // AutoAdjust.addjustElem(document.getElementById("messageElem"));
 
-/*
-AutoAdjust.addjustAll(function (check, styleData) {
+AutoAdjust.adjustAll(function (check, styleData) {
     console.log(styleData);
 
     if (check) {
@@ -23,4 +22,3 @@ AutoAdjust.addjustAll(function (check, styleData) {
         console.log("高さが変わりませんでした");
     }
 });
-*/
